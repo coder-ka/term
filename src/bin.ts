fe71eb6142d0283d076c46acab77c31956c09ece
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2026-01-23 13:04:16 +0900

    Initial Commit.

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,1 @@
 #!/usr/bin/env node
-
-import { add } from ".";
-
-console.log(add(1, 2)); // Output: 3
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,92 @@
-export function add(a: number, b: number) {
-  return a + b;
+/**
+ * Term is a reactive primitive with the following features:
+ *
+ * - lazy initialization
+ * - observable
+ * - holds dirty state
+ */
+export type Term<T> = {
+  (): T;
+  set(value: T): void;
+  subscribe(fn: (value: T) => void): () => void;
+  dirtyMap: WeakMap<object, boolean>;
+};
+
+export function createTerm<T>(): Term<T> {
+  let initialized = false;
+  let value: T;
+  let subscribers = new Set<(x: T) => void>();
+
+  return Object.assign<
+    {
+      // Callable form of getter
+      (): T;
+    },
+    Pick<Term<T>, "set" | "subscribe" | "dirtyMap">
+  >(
+    function get() {
+      if (!initialized) {
+        throw new Error("Term is not initialized");
+      }
+      return value;
+    },
+    {
+      set(newValue: T) {
+        value = newValue;
+        initialized = true;
+        for (const subscriber of subscribers) {
+          subscriber(newValue);
+        }
+      },
+      subscribe(fn: (value: T) => void) {
+        subscribers.add(fn);
+
+        return () => {
+          subscribers.delete(fn);
+        };
+      },
+      dirtyMap: new WeakMap<object, boolean>(),
+    },
+  );
+}
+
+export function createEffect(
+  fn: () => void | (() => void),
+  deps: Term<unknown>[],
+) {
+  let cleanup: void | (() => void) = undefined;
+
+  const unsubscribes = deps.map((dep) => {
+    dep.dirtyMap.set(fn, true);
+    return dep.subscribe(() => {
+      dep.dirtyMap.set(fn, true);
+    });
+  });
+
+  return {
+    invoke: () => {
+      const isDirty =
+        deps.length === 0 ||
+        deps.reduce((isDirty, dep) => {
+          const depIsDirty = dep.dirtyMap.get(fn) === true;
+          dep.dirtyMap.set(fn, false);
+          return isDirty || depIsDirty;
+        }, false);
+
+      if (!isDirty) return;
+
+      if (cleanup) {
+        cleanup();
+      }
+      cleanup = fn();
+    },
+    dispose: () => {
+      if (cleanup) {
+        cleanup();
+      }
+      for (const unsubscribe of unsubscribes) {
+        unsubscribe();
+      }
+    },
+  };
 }
